ae53c55fcf4eb64bc4de00627b06abb4492f18aa
Rodrigo Daniel Andrade <rodasistemas@gmail.com>
2019-08-08 21:37:12 -0300

    Create routes.js

---
+++ api/src/app/models/routes.js
@@ -0,0 +1,32 @@
+//===========================================================================
+// Requires
+//===========================================================================
+const db = require("../database/index");
+//===========================================================================
+var routes = db("routes");
+//===========================================================================
+// Schema
+//===========================================================================
+const schema = {
+    table: function(){
+        return db("routes");
+    },
+    list: function(){
+      return db("routes").then((data)=>{return data });
+    },
+    show: function(id){
+      return db("routes").where("id",id).then((data)=>{return data })
+    },
+    save: function(data){
+          var dados = data;
+          console.log("Salvando Dados");
+          db("routes").clearCounters().clearSelect().clearWhere();
+          console.log(dados);
+          return db("routes").insert(dados)
+            .then((data)=>{
+              return data;
+            });
+    }
+}
+//===========================================================================
+module.exports = schema;
