0eade30f3ea9271747d980d483fafe6438fad18c
Rodrigo Daniel Andrade <rodasistemas@gmail.com>
2019-08-09 13:41:47 -0300

    Update productsController.js

--- api/src/app/controllers/productsController.js
+++ api/src/app/controllers/productsController.js
@@ -29,8 +29,8 @@ router.post('/add',async(req,res,next)=>{
 router.post('/openroute',async(req,res,next)=>{
    const {dados} = req.body;
   console.log(dados);
-   const resp = await routes.openroute(dados).then((data)=>{return data});
-    res.send(resp);
+//    const resp = await routes.openroute(dados).then((data)=>{return data});
+//     res.send(resp);
 });
 router.post('/updateroute',async(req,res,next)=>{
    const {dados} = req.body;
