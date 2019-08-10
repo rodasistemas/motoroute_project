07a87d5020cb38353dccf12b8e5b334b38460db3
Rodrigo Daniel Andrade <rodasistemas@gmail.com>
2019-08-09 22:37:14 -0300

    Update freedrivers.js

--- api/src/app/models/freedrivers.js
+++ api/src/app/models/freedrivers.js
@@ -17,6 +17,9 @@ const schema = {
     show: function(id){
       return db("freedriver").where("id",id).then((data)=>{return data })
     },
+    delete: function(id){
+      return db("freedriver").where("id",id).del().then((data)=>{return data })
+    },
     update: function(dados){
         var id = dados.id;
         var data = dados.data;
