caae638e266c40649cddd9b336287d98004a8da9
Rodrigo Daniel Andrade <rodasistemas@gmail.com>
2019-08-09 23:54:50 -0300

    Create routedriver.js

---
+++ api/src/app/models/routedriver.js
@@ -0,0 +1,40 @@
+//===========================================================================
+// Requires
+//===========================================================================
+const db = require("../database/index");
+//===========================================================================
+var routedriver = db("routedriver");
+//===========================================================================
+// Schema
+//===========================================================================
+const schema = {
+    table: function(){
+        return db("routedriver");
+    },
+    list: function(){
+      return db("routedriver").then((data)=>{return data });
+    },
+    show: function(id){
+      return db("routedriver").where("id",id).then((data)=>{return data })
+    },
+    delete: function(id){
+      return db("routedriver").where(id).del().then((data)=>{return data })
+    },
+    update: function(dados){
+        var id = dados.id;
+        var data = dados.data;
+        return db("routedriver").where("id",id).update(data).then((data)=>{return data });
+    },
+    save: function(data){
+          var dados = data;
+          console.log("Salvando Dados");
+          db("routedriver").clearCounters().clearSelect().clearWhere();
+          console.log(dados);
+          return db("routedriver").insert(dados)
+            .then((data)=>{
+              return data;
+            });
+    }
+}
+//===========================================================================
+module.exports = schema;
