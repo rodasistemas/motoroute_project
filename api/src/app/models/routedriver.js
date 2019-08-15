2666fd9b02715a5e6da1224178f40bb8764b8ca4
rodasistemas <rodasistemas@gmail.com>
2019-08-15 04:06:14 -0300

    T

--- api/src/app/models/routedriver.js
+++ api/src/app/models/routedriver.js
@@ -15,7 +15,7 @@ const schema = {
       return db("routedriver").then((data)=>{return data });
     },
 showmin: function(id){
-      return db("routedriver").innerJoin("routes","routedriver.route_id","routes.id").where(id).order("routedriver.distance","ASC").then((data)=>{return data })
+      return db("routedriver").innerJoin("routes","routedriver.route_id","routes.id").where(id).orderBy("routedriver.distance","ASC").then((data)=>{return data })
     },
     show: function(id){
       return db("routedriver").innerJoin("routes","routedriver.route_id","routes.id").where(id).then((data)=>{return data })
